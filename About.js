1d37f31fb936773a66b00c616d510ef0fffa19a8
Your Name <you@example.com>
2023-11-02 06:55:25 -0700

    file changes

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className='about'>
+      <h1>About Us</h1>
+      <h2>Polar makes bills easy</h2>
+     
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/chm8kO1FY2U" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    </div>
+  );
+}
