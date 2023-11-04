e3b0bb97bb420f4ed6b2466f7ec0691437744453
Your Name <you@example.com>
2023-11-04 08:32:31 -0700

    Landing Page Changes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,31 +1,67 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import MapChart from './MapChart';
 
 const LandingPage = () => {
-  const [content, setContent] = useState("");
-  const history = useHistory();
+  const [selectedLevel, setSelectedLevel] = useState('federal');
+  const [selectedState, setSelectedState] = useState(null);
+  const [selectedCounties, setSelectedCounties] = useState([]);
+  const navigate = useNavigate();
 
-  const handleStateClick = (stateName) => {
-    // Redirect to the homepage with filtered data for the selected state
-    history.push(`/homepage?level=state&state=${stateName}`);
+  const handleStateSelection = (stateId) => {
+    setSelectedLevel('state');
+    setSelectedState(stateId);
+    setSelectedCounties([]); // Clear any county selections
   };
-  
-    const navigateToHome = (level) => {
-        history.push(`/home/${level}`);
-    };
 
-  // Handler for selecting federal level
-  const handleFederalClick = () => {
-    history.push(`/homepage?level=federal`);
+  const handleCountySelection = (countyId) => {
+    // Toggle county selection
+    setSelectedCounties(prevCounties =>
+      prevCounties.includes(countyId)
+        ? prevCounties.filter(id => id !== countyId)
+        : [...prevCounties, countyId]
+    );
+  };
+
+  const handleSelectAllStates = () => {
+    setSelectedLevel('federal');
+    setSelectedState(null);
+    setSelectedCounties([]);
+  };
+
+  const handleSelectAllCounties = () => {
+    // This should be replaced with actual logic to select all counties within the state
+    alert('Select all counties within the state is not implemented yet.');
+  };
+
+  const handleConfirmSelection = () => {
+    if (selectedLevel === 'federal') {
+      navigate('/home', { state: { level: 'federal' } });
+    } else if (selectedLevel === 'state' && selectedState) {
+      navigate('/home', { state: { level: 'state', stateId: selectedState } });
+    } else if (selectedLevel === 'county' && selectedCounties.length > 0) {
+      navigate('/home', { state: { level: 'county', countyIds: selectedCounties } });
+    }
   };
 
   return (
     <div>
-      <MapChart setTooltipContent={setContent} setClickedState={handleStateClick} />
-      <div className="tooltip">{content}</div>
-      <button onClick={handleFederalClick}>View Federal Legislation</button>
-      {/* Additional UI elements for filtering */}
+      <MapChart
+        selectedLevel={selectedLevel}
+        onStateSelected={handleStateSelection}
+        onCountySelected={handleCountySelection}
+        selectedState={selectedState}
+        selectedCounties={selectedCounties}
+      />
+      <div>
+        {selectedLevel === 'federal' && (
+          <button onClick={handleSelectAllStates}>Select All States</button>
+        )}
+        {selectedLevel === 'state' && (
+          <button onClick={handleSelectAllCounties}>Select All Counties</button>
+        )}
+        <button onClick={handleConfirmSelection}>Confirm Selection</button>
+      </div>
     </div>
   );
 };
--- src/components/MapChart.js
+++ src/components/MapChart.js
@@ -1,47 +1,74 @@
-import React from "react";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import usAtlas from "us-atlas/states-10m.json"; // Import the TopoJSON for the US map
+import React, { useState, useEffect } from 'react';
+import { ComposableMap, Geographies, Geography, ZoomableGroup } from 'react-simple-maps';
+import usAtlasStates from "us-atlas/states-10m.json";
+import usAtlasCounties from "us-atlas/counties-10m.json";
+import { feature } from 'topojson-client';
+import { geoCentroid } from 'd3-geo';
 
-const MapChart = ({ setTooltipContent, setClickedState }) => {
+const MapChart = ({ selectedLevel, onStateSelected, onCountySelected, selectedState, selectedCounties }) => {
+  const [geographies, setGeographies] = useState(feature(usAtlasStates, usAtlasStates.objects.states).features);
+  const [zoom, setZoom] = useState(1);
+  const [center, setCenter] = useState([0, 0]);
+
+  useEffect(() => {
+    if (selectedLevel === 'state' && selectedState) {
+      const stateGeographies = feature(usAtlasStates, usAtlasStates.objects.states).features;
+      const selectedGeo = stateGeographies.find(geo => geo.id === selectedState);
+      if (selectedGeo) {
+        const centroid = geoCentroid(selectedGeo);
+        setCenter(centroid);
+        setZoom(4); // Adjust zoom level as needed
+        setGeographies(feature(usAtlasCounties, usAtlasCounties.objects.counties).features.filter(geo => geo.id.startsWith(selectedState)));
+      } else {
+        console.error('Selected state geo data is not valid:', selectedGeo);
+      }
+    } else {
+      setCenter([0, 0]);
+      setZoom(1);
+      setGeographies(feature(usAtlasStates, usAtlasStates.objects.states).features);
+    }
+  }, [selectedLevel, selectedState]);
+
   return (
     <ComposableMap projection="geoAlbersUsa">
-      <Geographies geography={usAtlas}>
-        {({ geographies }) =>
-          geographies.map(geo => (
-            <Geography
-              key={geo.rsmKey}
-              geography={geo}
-              onMouseEnter={() => {
-                // Note: The property might be named differently, you might need to adjust this
-                const { name } = geo.properties;
-                setTooltipContent(name);
-              }}
-              onMouseLeave={() => {
-                setTooltipContent("");
-              }}
-              onClick={() => {
-                // Note: The property might be named differently, you might need to adjust this
-                const { name } = geo.properties;
-                setClickedState(name);
-              }}
-              style={{
-                default: {
-                  fill: "#D6D6DA",
-                  outline: "none"
-                },
-                hover: {
-                  fill: "#F53",
-                  outline: "none"
-                },
-                pressed: {
-                  fill: "#E42",
-                  outline: "none"
-                }
-              }}
-            />
-          ))
-        }
-      </Geographies>
+      <ZoomableGroup center={center} zoom={zoom}>
+        <Geographies geography={geographies}>
+          {({ geographies }) =>
+            geographies.map(geo => {
+              const isSelected = selectedLevel === 'state'
+                ? geo.id === selectedState
+                : selectedCounties.includes(geo.id);
+              return (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onClick={() => {
+                    if (selectedLevel === 'federal') {
+                      onStateSelected(geo.id);
+                    } else if (selectedLevel === 'state') {
+                      onCountySelected(geo.id);
+                    }
+                  }}
+                  style={{
+                    default: {
+                      fill: isSelected ? "#FFD700" : "#D6D6DA",
+                      outline: "none"
+                    },
+                    hover: {
+                      fill: isSelected ? "#FFD700" : "#F53",
+                      outline: "none"
+                    },
+                    pressed: {
+                      fill: "#E42",
+                      outline: "none"
+                    }
+                  }}
+                />
+              );
+            })
+          }
+        </Geographies>
+      </ZoomableGroup>
     </ComposableMap>
   );
 };
